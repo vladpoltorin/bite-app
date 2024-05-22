30672e99b90d261b1883997b4af93be968c00262
Vlad Poltorin <vlad@getoctane.io>
2024-05-22 15:56:31 -0400

    refactor: update product card with Chakra components

--- src/ProductCard.tsx
+++ src/ProductCard.tsx
@@ -1,13 +1,45 @@
+import {
+  Button,
+  ButtonGroup,
+  Card,
+  CardBody,
+  CardFooter,
+  Divider,
+  Heading,
+  Stack,
+  Text,
+  Image,
+} from '@chakra-ui/react';
 import type { Product } from './types';
 
-export const ProductCard = ({ id, price, name, description, imageUrl }: Product) => {
+export const ProductCard = ({ price, name, description, imageUrl }: Product) => {
   return (
-    <div>
-      <img src={imageUrl} alt={`${name} product card`} width={200} height={150} />
-      <p>{id}</p>
-      <p>{name}</p>
-      <p>{description}</p>
-      <p>{price}</p>
-    </div>
+    <Card maxW='sm'>
+      <CardBody>
+        <Image
+          src={imageUrl}
+          alt={`${name} card`}
+          borderRadius='lg'
+        />
+        <Stack mt='6' spacing='3'>
+          <Heading size='md'>Living room Sofa</Heading>
+          <Text>{description}</Text>
+          <Text color='blue.600' fontSize='2xl'>
+            ${price}
+          </Text>
+        </Stack>
+      </CardBody>
+      <Divider />
+      <CardFooter>
+        <ButtonGroup spacing='2' justifyContent="space-between" w="full">
+          <Button variant="outline" colorScheme='blue'>
+            Details
+          </Button>
+          <Button variant='solid' colorScheme='blue'>
+            Add to cart
+          </Button>
+        </ButtonGroup>
+      </CardFooter>
+    </Card>
   );
 };
--- src/ProductList.tsx
+++ src/ProductList.tsx
@@ -1,13 +1,15 @@
+import { HStack } from '@chakra-ui/react';
 import type { Product } from './types';
 import products from './data.json';
 import { ProductCard } from './ProductCard';
 
+
 export const ProductList = () => {
   return (
-    <div>
+    <HStack wrap="wrap" gap={5} alignItems="stretch">
       {products.map((product: Product) => (
         <ProductCard key={product.id} {...product} />
       ))}
-    </div>
+    </HStack>
   );
 };
